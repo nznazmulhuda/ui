ee85c283347c79b8794d6689785d931601ed4839
Nazmul Huda <nznazmulhuda04@gmail.com>
2025-03-01 23:08:11 +0600

    ui first commit

---
+++ uxpin.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  components: {
+    categories: [
+      {
+        name: "Components",
+        include: [
+          "src/components/**/*.{js,jsx,ts,tsx}",  // Match all component files in the components folder
+        ],
+      },
+    ],
+  },
+  name: "Grafity UI Library",
+};
